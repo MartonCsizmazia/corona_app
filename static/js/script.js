7e0b1b6fe33ab41784a23328e17f9eae869eef96
MartonCsizmazia <csmarton93@gmail.com>
2020-04-07 10:24:12 +0200

    gitignore modified, static folder added

---
+++ static/js/script.js
@@ -0,0 +1,44 @@
+const actorRows = document.getElementsByClassName('doctor-row');
+
+for (let row of actorRows) {
+    row.addEventListener("click", clickHandler);
+
+    function clickHandler() {
+        let age = row.querySelector(".age-val");
+        let name = row.querySelector(".actor-name");
+        if (age.innerHTML >= 50){
+            name.innerHTML = "Old" + name.innerHTML
+        }
+        else{
+            name.innerHTML = "Young" + name.innerHTML
+        }
+        row.removeEventListener("click", clickHandler);
+    }
+
+}
+/*
+    kűlső skope változóit minimalizálni
+ */
+
+
+/*
+ {
+ var elem1 = document.getElementById('idDiv3');
+ var elem2 = elem1.closest("#idDiv2");
+ alert(elem2.outerHTML);
+ }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
