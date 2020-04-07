05899b09789335931dab47dc7314111cf0342180
MartonCsizmazia <csmarton93@gmail.com>
2020-04-07 09:30:57 +0200

    first commit in pycharm

--- app.py
+++ app.py
@@ -11,21 +11,23 @@ app = Flask(__name__)
 def main_page():  # Just a normal function, I named it this way for cleaner code
     # Okay, we render the index.html, then return the html string.
 
-	d = pd.read_html('https://koronavirus.gov.hu/elhunytak')
-	df = d[0]
+    d = pd.read_html('https://koronavirus.gov.hu/elhunytak')
+    df = d[0]
 
-	# converting to list 
-	#meg kell adni az oszlop nevét, itt valamiért nagyon rosszul fogadta csak be
-	#ageList = df.loc[:,'\n          Kor        '].tolist()
-	ageList = df[df.columns[2]].tolist()
+    # converting to list
+    # in normal you have to give the coulms name, in this it accepted only this way
+    # ageList = df.loc[:,'\n          Kor        '].tolist()
+    ageList = df[df.columns[2]].tolist()
 
-	average = math.floor(sum(ageList)/len(ageList))
+    average = math.floor(sum(ageList) / len(ageList))
 
-	#return 'Az elhunytak átlagéletkora:   %d' % average
+    # return 'Az elhunytak atlageletkora:   %d' % average
 
-	return render_template('index.html', average=average)
+    return render_template('index.html', average=average)
+
+
 if __name__ == "__main__":
     app.run(
-        debug=True, # Allow verbose error reports
-        port=5000 # Set custom port
+        debug=True,  # Allow verbose error reports
+        port=5000  # Set custom port
     )
