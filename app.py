dd8c1c2b62613cb1ddaa0e6c6ced0796f0c6251f
MartonCsizmazia <csmarton93@gmail.com>
2020-04-10 12:26:03 +0200

    cards added, most common sickness data created

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import math
+from collections import Counter
+import operator
+
 
 from flask import Flask, render_template, request, jsonify
 
@@ -18,6 +21,7 @@ def main_page():  # Just a normal function, I named it this way for cleaner code
     # in normal you have to give the coulms name, in this it accepted only this way
     # ageList = df.loc[:,'\n          Kor        '].tolist()
     ageList = df[df.columns[2]].tolist()
+    tempSicknessList = df[df.columns[3]].tolist()
 
     x = 0
     urlBase = 'https://koronavirus.gov.hu/elhunytak?page='
@@ -26,16 +30,43 @@ def main_page():  # Just a normal function, I named it this way for cleaner code
             x += 1
             url = urlBase + str(x)
             d = pd.read_html(url)
-            df2 = d[0]
-            ageList2 = df2[df2.columns[2]].tolist()
+            df = d[0]
+            ageList2 = df[df.columns[2]].tolist()
+            sicknessList2 = df[df.columns[3]].tolist()
             ageList += ageList2
+            tempSicknessList += sicknessList2
         except ValueError:
             break
 
-    average = math.floor(sum(ageList) / len(ageList))
-    # return 'Az elhunytak atlageletkora:   %d' % average
+    # print(df)
+    sicknessList = []
 
-    return render_template('index.html', average=average)
+    for string in tempSicknessList:
+        if "," in string:
+            sliced = string.split(", ")
+            for word in sliced:
+                sicknessList.append(word)
+        else:
+            sicknessList.append(string)
+    print(sicknessList[0])
+
+    how_many = dict(Counter(sicknessList))
+
+    how_many = sorted(how_many.items(), key=operator.itemgetter(1), reverse=True)
+    most_common = []
+
+    HOW_MUCH_MOST_COMMON = 3
+    for idx, sickness in enumerate(how_many):
+        if idx < HOW_MUCH_MOST_COMMON:
+            print(sickness[0])
+            most_common.append(sickness)
+        else:
+            break
+
+    print(most_common)
+    average = math.floor(sum(ageList) / len(ageList))
+
+    return render_template('index.html', average=average, most_common=most_common, number_of_deaths=len(ageList))
 
 
 # A welcome message to test our server
